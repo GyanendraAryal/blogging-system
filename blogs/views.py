4a1cbcee49ea0074266f31b54ccde360ba10c2a7
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-09-01 12:28:39 +0545

    post by category and template inheritance

--- blogs/views.py
+++ blogs/views.py
@@ -7,12 +7,12 @@ from .models import Blog, Category
 def posts_by_category(request, id):
     # Fetch the post that belongs to the category with the id:id
     posts = Blog.objects.filter(status="Published", category=id)
-    try:
-        category = Category.objects.get(id=id)
-    except:
-        # redirect to home
-        return redirect("home")
-    # category = get_object_or_404(Category, id=id)
+    # try:
+    #     category = Category.objects.get(id=id)
+    # except:
+    #     # redirect to home
+    #     return redirect("home")
+    category = get_object_or_404(Category, id=id)
     # use get_object_or_404() to show custom 404 page
     context = {
         "posts": posts,
