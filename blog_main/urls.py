0f0d0729db20ebba58381e961c45e7b8966e3536
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-09-02 22:07:44 +0545

    feat: login and logout

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from .views import home, register, login
-from django.urls import path,include
+from .views import home, register, login, logout
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from blogs import views as BlogsView
@@ -29,6 +29,7 @@ urlpatterns = [
     path("blogs/<slug:slug>/", BlogsView.blogs, name="blogs"),
     # Search Endpoint
     path("blogs/search/", BlogsView.search, name="search"),
-    path("register/",register, name="register"),
-    path("login/", login, name="login")
+    path("register/", register, name="register"),
+    path("login/", login, name="login"),
+    path("logout/", logout, name="logout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
