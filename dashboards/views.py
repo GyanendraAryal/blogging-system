90aa47717b2cd0274f5648be0bfc8121aa16aa0e
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-09-05 16:59:05 +0545

    dynamic dashboard

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from blogs.models import Category, Blog
 
 
 # Create your views here.
 def dashboard(request):
-    return render(request, "dashboard/dashboard.html")
+    category_count = Category.objects.all().count()
+    blogs_count = Blog.objects.all().count()
+    context = {
+        "category_count": category_count,
+        "blogs_count": blogs_count
+    }
+    # print(categories, blogs)
+    return render(request, "dashboard/dashboard.html", context)
