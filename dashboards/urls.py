a39ac0d0753e2e760a7121bcbd0daac44804e88e
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-09-06 14:18:38 +0545

    crud implementation on category dashboard

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path("categories/", views.categories, name="categories"),
+    path("categories/add/", views.add_category, name="add_category"),
+    path("categories/edit/<int:id>/", views.edit_category, name="edit_category"),
+    path("categories/delete/<int:id>/", views.delete_category, name="delete_category"),
 ]
